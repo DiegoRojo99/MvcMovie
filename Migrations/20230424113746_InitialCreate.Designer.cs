a58bf7b798854554ff5f14e0bd4b8e30c52a06ee
Diego Rojo <Diego99rojo@gmail.com>
2023-04-24 12:46:13 +0100

    Added rating to the movies

--- Migrations/20230424113746_InitialCreate.Designer.cs
+++ Migrations/20230424113746_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace McvMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20230424101657_InitialCreate")]
+    [Migration("20230424113746_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace McvMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
